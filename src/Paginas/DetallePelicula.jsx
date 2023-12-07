2ccaf9d5a93563636fa5d1d0b74aabd2fc5f34ac
Jose V. Fernández <avefenixmas@gmail.com>
2023-12-07 18:06:41 +0100

    Añadido la fotografia y el CSS a la moviedetalles

--- src/Paginas/DetallePelicula.jsx
+++ src/Paginas/DetallePelicula.jsx
@@ -4,12 +4,15 @@ import "./DetallePelicula.css"
 
 export const DetallePelicula = ({ data, error }) => {
   return (
-    <div>
+    <div className= "DetallePelicula">
       {data ? (
         <div>
           <h1>{`Movie ${data.title}`}</h1>
           <section>
-            
+            <img
+              src={data.poster_path ? `https://image.tmdb.org/t/p/original${data.poster_path}` : imagenPorDefecto}
+              alt={data.title}
+            />
             <p>Sipnosis: {data.overview}</p>
           </section>
         </div>
--- src/Paginas/MejorValoradas/MejorValoradas.jsx
+++ src/Paginas/MejorValoradas/MejorValoradas.jsx
@@ -2,7 +2,7 @@ import './MejorValoradas.css'
 
 import React, { useEffect, useState } from 'react';
 import { ModuloPelicula } from '../ModuloPelicula';
-import { DetallePelicula } from '../DetallePelicula'; 
+import { DetallePelicula } from '../DetallePelicula';
 
 
 
@@ -33,46 +33,46 @@ export const MejorValoradas = () => {
         setMoviesData(Array.isArray(results) ? results.slice(0, 12) : []);
       })
       .catch(err => console.error(err));
-}, []);
+  }, []);
 
-const handleMovieClick = async (identificador) => {
-  setSelectedMovie(identificador);
+  const handleMovieClick = async (identificador) => {
+    setSelectedMovie(identificador);
 
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${identificador}?api_key=${apiKey}`);
+    try {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${identificador}?api_key=${apiKey}`);
 
-    if (response.status === 200) {
-      const data = await response.json();
-      setMovieDetails({ data, error: null });
-    } else {
-      console.error(response);
-      throw new Error('Algo no funcionó');
+      if (response.status === 200) {
+        const data = await response.json();
+        setMovieDetails({ data, error: null });
+      } else {
+        console.error(response);
+        throw new Error('Algo no funcionó');
+      }
+    } catch (error) {
+      setMovieDetails({ data: null, error: error.message });
     }
-  } catch (error) {
-    setMovieDetails({ data: null, error: error.message });
-  }
-};
+  };
 
 
   return (
     <div>
       <h1 className="TituloPrincipal">Las 12 Películas mejor Valoradas</h1>
-        <div className = "MejorValoradasBoxDesign">
-          <div className="PeliculasColumn">
-              {moviesData.map(movie => (
-                <ModuloPelicula
-                  key={movie.id}
-                  title={movie.title}
-                  backdropPath={movie.backdrop_path}
-                  onClick={() => handleMovieClick(movie.id)}
-                />
-              ))}
-          </div>
-
-          <div className="DetalleColumn">
-          {selectedMovie && <DetallePelicula data={movieDetails.data} error={movieDetails.error} />}
-          </div>
+      <div className="MejorValoradasBoxDesign">
+        <div className="PeliculasColumn">
+          {moviesData.map(movie => (
+            <ModuloPelicula
+              key={movie.id}
+              title={movie.title}
+              backdropPath={movie.backdrop_path}
+              onClick={() => handleMovieClick(movie.id)}
+            />
+          ))}
         </div>
+
+        <div className="DetalleColumn">
+          {selectedMovie && <DetallePelicula data={movieDetails.data} error={movieDetails.error} />}
+        </div>
+      </div>
     </div>
   );
 };
